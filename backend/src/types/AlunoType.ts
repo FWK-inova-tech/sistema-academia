0ac4a144b22bf293caed125e5bdd3f0c4a25b950
Mayk Lucas <maykads2018@gamil.com>
2025-07-07 17:32:07 -0300

    Creating a file that will define the interfaces for your types

---
+++ backend/src/types/AlunoType.ts
@@ -0,0 +1,46 @@
+export interface PerimetriaType {
+    bracoDireito: number;
+    bracoEsquerdo: number;
+    coxaDireita: number;
+    coxaEsquerda: number;
+    panturrilhaDireita: number;
+    panturrilhaEsquerda: number;
+    peito: number;
+    cintura: number;
+    quadril: number;
+    dataMedicao: Date;
+}
+
+export interface TreinoItemType {
+    exercicio: string;
+    series: string;
+    repeticoes: string;
+    carga: string;
+    observacoes: string;
+}
+
+export interface TreinoType {
+    nomeTreino: string;
+    dataTreino: Date;
+    intensidade: string;
+    exercicio: TreinoItemType[];
+    observacoesGerais?: string;
+}
+
+export type NivelAluno = 'Iniciante' | 'Intermediário' | 'Avançado';
+
+export interface AlunoType {
+    nome: string;
+    objetivo: string;
+    dataNascimento: Date;
+    professor: string;
+    nivel: NivelAluno;
+    contato: string;
+    dataInicio: Date;
+    dataRevisao: Date;
+    anaminese: string;
+    id?: string;
+    agenda: string[];
+    treino: TreinoType[];
+    perimetria: PerimetriaType[];
+}
