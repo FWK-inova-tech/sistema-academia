a1027528b1dcbbf81264d2aad26f2569ae9ff1ce
elen <elenndev@gmail.com>
2025-07-23 21:43:49 -0300

    feat: redirect to /login when fetch api returns error 401

--- src/components/students/@Students.tsx
+++ src/components/students/@Students.tsx
@@ -9,113 +9,115 @@ import { Loading } from "../Loading";
 
 interface studentsProps {
   currentStudentsList: Pick<AlunoType, '_id' | 'nome'>[];
-  setError: ( error: {
+  setError: (error: {
     message: string;
-    callback: ()=> Promise<void> | void;
+    callback: () => Promise<void> | void;
   }) => void;
   controlOpenSheet: (action: 'close' | 'open') => void;
 }
-export const Students = ({ setError, currentStudentsList, controlOpenSheet } : studentsProps) => {
+export const Students = ({ setError, currentStudentsList, controlOpenSheet }: studentsProps) => {
   const [openEdit, setOpenEdit] = useState(false)
-  
-  const loading = useAppSelector((state)=> state.students.loading)
+
+  const loading = useAppSelector((state) => state.students.loading)
 
   // currentStudentSheet == false => nenhuma ficha de aluno aberta, mostrar lista normalmente
   // currentStudentSheet == null => tentou abrir a ficha mas nenhum aluno com aquele id foi retornado 
   const [currentStudentSheet, setCurrentStudentSheet] = useState<false | AlunoType | null>(false)
   const dispatch = useAppDispatch()
 
-  async function handleOpenStudentSheet(id: string){
+  async function handleOpenStudentSheet(id: string) {
     dispatch(setLoading("Buscando dados do aluno"))
-    try{
+    try {
       const req = await getAluno(id)
-      if(req){
+      if (req) {
         dispatch(setLoading(false))
         setCurrentStudentSheet(req)
       } else {
         setCurrentStudentSheet(null)
       }
-    }catch(error){
-      const errorMessage = error instanceof Error? error.message : "Erro na requisição para buscar a lista de alunos"
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : "Erro na requisição para buscar os dados do aluno"
       setError({
-        message: errorMessage, 
-        callback: () => {handleOpenStudentSheet(id)}})
+        message: errorMessage,
+        callback: () => { handleOpenStudentSheet(id) }
+      })
     }
   }
 
-  function handleCloseStudentSheet(){
+  function handleCloseStudentSheet() {
     setCurrentStudentSheet(false)
     controlOpenSheet('close')
   }
 
-  function handleOpenEdit(){
+  function handleOpenEdit() {
     controlOpenSheet('open')
     setOpenEdit(true)
   }
-  
-  function handleCloseEdit(){
+
+  function handleCloseEdit() {
     setOpenEdit(false)
   }
 
 
   return (
     <>
-      { loading ? 
-        <Loading loadingMessage={loading}/>
-      : <>
-        {currentStudentSheet ? <>
-          {openEdit ? 
-            <StudentForm 
-            currentStudentSheet={{student: currentStudentSheet, updateCurrentStudentSheet: setCurrentStudentSheet}}
-            closeForm={handleCloseEdit}/> 
-            : <>
-            <button type="button"
-            onClick={handleCloseStudentSheet}>
-              Voltar para a lista de alunos
-            </button>
-            <StudentSheet
-            closeStudentSheet={handleCloseStudentSheet}
-            currentStudentSheet={currentStudentSheet} 
-            openEdit={handleOpenEdit}/>
-          </>}
-        </>
-        : currentStudentSheet !== false ? <p>Não foi encontrado nenhum aluno com o id informado</p> 
-        : 
-        <div className="content students-list">
-          <h1>Alunos</h1>
-        {currentStudentsList.length > 0 ? 
-          <table className="students-list">
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Nome</th>
-                <th>Ações</th>
-              </tr>
-            </thead>
-            <tbody>
-              {currentStudentsList.map(student => (
-                <tr key={student._id}>
-                  <td className={'id'} title={student._id}>
-                    #{student._id.slice(0, 7)}...
-                  </td>
-                  <td>{student.nome}</td>
-                  <td className="student-actions">
-                    <button
-                    className='btn-green' 
-                    type='button'
-                    onClick={()=>{
-                      handleOpenStudentSheet(student._id)
-                      controlOpenSheet('open')}}>
-                      Abrir ficha
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          : <p>Não há nenhum aluno registrado</p>}
-          </div>
-        }
+      {loading ?
+        <Loading loadingMessage={loading} />
+        : <>
+          {currentStudentSheet ? <>
+            {openEdit ?
+              <StudentForm
+                currentStudentSheet={{ student: currentStudentSheet, updateCurrentStudentSheet: setCurrentStudentSheet }}
+                closeForm={handleCloseEdit} />
+              : <>
+                <button type="button"
+                  onClick={handleCloseStudentSheet}>
+                  Voltar para a lista de alunos
+                </button>
+                <StudentSheet
+                  closeStudentSheet={handleCloseStudentSheet}
+                  currentStudentSheet={currentStudentSheet}
+                  openEdit={handleOpenEdit} />
+              </>}
+          </>
+            : currentStudentSheet !== false ? <p>Não foi encontrado nenhum aluno com o id informado</p>
+              :
+              <div className="content students-list">
+                <h1>Alunos</h1>
+                {currentStudentsList.length > 0 ?
+                  <table className="students-list">
+                    <thead>
+                      <tr>
+                        <th>Id</th>
+                        <th>Nome</th>
+                        <th>Ações</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {currentStudentsList.map(student => (
+                        <tr key={student._id}>
+                          <td className={'id'} title={student._id}>
+                            #{student._id.slice(0, 7)}...
+                          </td>
+                          <td>{student.nome}</td>
+                          <td className="student-actions">
+                            <button
+                              className='btn-green'
+                              type='button'
+                              onClick={() => {
+                                handleOpenStudentSheet(student._id)
+                                controlOpenSheet('open')
+                              }}>
+                              Abrir ficha
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                  : <p>Não há nenhum aluno registrado</p>}
+              </div>
+          }
         </>
       }
     </>
