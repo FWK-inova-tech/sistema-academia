af30d84a8c2c801f832f6305fd4b2e3d72d2c740
elen <elenndev@gmail.com>
2025-09-14 19:01:49 -0300

    fix: update constatnt newStudentInitialValue

--- src/constants/newStudentInitialValue.ts
+++ src/constants/newStudentInitialValue.ts
@@ -3,18 +3,19 @@ import { itensPerimetria } from "./medidasPerimetria";
 
 export const newStudentInitialValue: Omit<AlunoType, '_id'> = {
   nome: '',
-          objetivo: '',
-          dataNascimento: new Date(),
-          professor: '',
-          nivel: "Iniciante",
-          contato: '',
-          dataInicio: new Date(),
-          dataRevisao: new Date(),
-          anaminese: '',
-          agenda: [],
-          treino: [],
-          perimetria: {
-            data: new Date(),
-            medidas: itensPerimetria
-          }
+  objetivo: '',
+  dataNascimento: new Date(),
+  professor: '',
+  nivel: "Iniciante",
+  contato: '',
+  dataInicio: new Date(),
+  dataRevisao: new Date(),
+  anaminese: '',
+  agenda: [],
+  status: 'active',
+  treino: [],
+  perimetria: {
+    data: new Date(),
+    medidas: itensPerimetria
+  }
 }
